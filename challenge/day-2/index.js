53a2a5f5ec18840beac85b0de6d3ff107c572859
Djaglo Doris <dorisdjalo@gmail.com>
2023-05-11 07:27:17 +0000

    Update: index.js updated

--- challenge/day-2/index.js
+++ challenge/day-2/index.js
@@ -2,7 +2,7 @@
 const path = window.location.pathname;
 
 // check if the path matches '/day-2' or '/day-3'
-if (path === 'https://github.com/djaglodoris/100-days-of-code/main/challenge/day-2' || path === 'https://github.com/djaglodoris/100-days-of-code/main/challenge/day-3') {
+if (path === '/challenge/day-2' || path === '/challenge/day-3') {
     // redirect to the homepage
-    window.location.href = 'djaglodoris.github.io/100-days-of-code/';
+    window.location.href = '/';
 }
